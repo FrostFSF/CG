b22351d2409a94209bc72702cfc010b4b373f34e
FrostFSF <113884682+FrostFSF@users.noreply.github.com>
2022-12-31 13:08:07 +0530

    Create bresCircle.cpp

---
+++ bresCircle.cpp
@@ -0,0 +1,50 @@
+#include <iostream.h>
+#include <graphics.h>
+#include <conio.h>
+#include <math.h>
+
+void bres_circle(int xc,int yc,int radius)    //xc,yc is center of the circle where you want to put the circle center.
+{
+  int x,y;
+  x = 0;
+  y = radius;
+  
+  int d;
+  d = 3-2*radius;
+  
+  while(x < y)
+  {
+    pitpixel(xc+x,yc+y, WHITE);
+    pitpixel(xc-x,yc-y, WHITE);
+    pitpixel(xc+x,yc-y, WHITE);
+    pitpixel(xc-x,yc+y, WHITE);
+    
+    pitpixel(xc+y,yc+x, WHITE);
+    pitpixel(xc-y,yc-x, WHITE);
+    pitpixel(xc+y,yc-x, WHITE);
+    pitpixel(xc-y,yc+x, WHITE);
+    if(d < 0)
+    {
+        d = d+4*dx+6;
+    }
+    else
+    {
+      d = d+4*(dx-dy)+10;
+      y = y-1;
+    }
+    x = x+1;
+  }
+}
+
+
+
+int main()
+{
+  int gd = DETECT,gm;
+  initgraph(&gd,&gm,"c://turboc3//bgi");
+  
+  bres_circle(200,200,30);
+  
+  getch();
+  return 0;
+}
